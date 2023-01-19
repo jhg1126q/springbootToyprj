6fea30bceb20c816cee3e44d694bd35c46fa74f8
Gustv <55569671+Gustv1126@users.noreply.github.com>
2023-01-20 00:15:18 +0900

    gjh-2023-01-19-03
    로또 끝

--- springboottest/src/main/java/org/springboottest/Main.java
+++ springboottest/src/main/java/org/springboottest/Main.java
@@ -2,17 +2,21 @@ package org.springboottest;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    private static final int TICKET_COUNT = 5;
+    private static final int TICKET_COUNT = 100;
     public static void main(String[] args) {
         ArrayList<Integer> total = new ArrayList<Integer>();
         ArrayList<Integer> result = new ArrayList<Integer>();
-        ArrayList<Integer>[] ticket = new ArrayList[TICKET_COUNT];
+        List<ArrayList<Integer>> ticket = new ArrayList<>();
         total = generateTotalNumbers(total);
         result = generateResultNumbers(total, result);
         printResultNumbers(result);
         ticket = buyTicket(total, ticket);
+        for(ArrayList<Integer> list: ticket) {
+            matchNumbers(list, result);
+        }
     }
 
     private static ArrayList<Integer> generateTotalNumbers(ArrayList<Integer> total) {
@@ -32,6 +36,7 @@ public class Main {
     private static void printResultNumbers(ArrayList<Integer> result) {
         System.out.println("result : ");
         result.forEach(number -> System.out.print("|" + number +  "|"));
+        System.out.println("\n------");
     }
 
     private static ArrayList<Integer> generateTicketNumbers(ArrayList<Integer> total, ArrayList<Integer> ticket) {
@@ -41,17 +46,48 @@ public class Main {
         return ticket;
     }
 
-    private static ArrayList<Integer>[] buyTicket(ArrayList<Integer> total, ArrayList<Integer>[] ticket) {
+    private static List<ArrayList<Integer>> buyTicket(ArrayList<Integer> total, List<ArrayList<Integer>> ticket) {
+        ArrayList<Integer> newTicket = new ArrayList<Integer>();
         for(int i =0; i < TICKET_COUNT; i++) {
-            ticket[i] = generateTicketNumbers(total, ticket[i]);
-            printTicketNumbers(ticket[i], i);
+            newTicket = generateTicketNumbers(total, newTicket);
+            ticket.add(newTicket);
+            newTicket = new ArrayList<Integer>();
+            printTicketNumbers(ticket.get(i), i);
         }
         return ticket;
     }
 
-    private static void printTicketNumbers(ArrayList<Integer> ticket, int num) {
+    private static void printTicketNumbers(ArrayList<Integer> soloTicket, int num) {
         System.out.println("Ticket " + num + " : ");
-        ticket.forEach(number -> System.out.print("|" + number +  "|"));
+        soloTicket.forEach(number -> System.out.print("|" + number +  "|"));
+        System.out.println("\n------");
     }
 
+    private static void matchNumbers(ArrayList<Integer> soloTicket, ArrayList<Integer> result) {
+        int matchPoint = 0;
+        for (int ticketNum : soloTicket) {
+            for (int resultNum : result) {
+                if(ticketNum == resultNum) {
+                    matchPoint += 1;
+                }
+            }
+        }
+        switch (matchPoint) {
+            case 3:
+                System.out.println("Winning : 5 rank ");
+                break;
+            case 4:
+                System.out.println("Winning : 4 rank ");
+                break;
+            case 5:
+                System.out.println("Winning : 3 rank ");
+                break;
+            case 6:
+                System.out.println("Winning : 2 rank ");
+                break;
+            default:
+                System.out.println("Lose : Boom ");
+                break;
+        }
+    }
 }
